5e1d324573fa1ec9afdb2177ef53fa76853abc7d
Mqy2000 <Ma_qingyuan@outlook.com>
2019-07-09 21:13:23 +0800

    the first change

--- a.cpp
+++ a.cpp
@@ -1,1 +1,21 @@
- 
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+	int dividend;
+	scanf("%d",&dividend);
+	int a = 2,b = sqrt(dividend);
+	while(!(dividend%a)){
+		printf("%d\n",a);
+		dividend /= a;
+	}
+	for(a = 3;a<b;a+=2){
+		while(!(dividend%a)){
+			printf("%d\n",a);
+			dividend /= a;
+		}
+	}
+	getchar();
+	getchar();
+	return 0;
+} 
